02a59eb218c4a82c6a54132f6e834bf2f4ef8b03
Sarthak Gupta <smartsarthakgupta12345@gmail.com>
2024-05-13 00:33:36 +0530

    qr added & footer fixed

--- src/Componet/Footer.jsx
+++ src/Componet/Footer.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter, FaStar } from 'react-icons/fa6';
 import './Footer.css';
 import { Link } from 'react-router-dom';
+import img from "../image/footer/footer-qr.jpg";
 
 function Footer() {
   return (
@@ -11,24 +12,15 @@ function Footer() {
       {' '}
       <div className="page_2 footer">
         <div className="footer-wrapper">
+
           <div className="flex">
             <h3 className="footer-title">CareerZunction</h3>
             <p className="footer-pera">
               CareerZunction is a dynamic platform connecting eager students
               with valuable internship opportunities.
             </p>
-            <div className="footer-socail-icon">
-              <a href="https://github.com/HimanshuNarware" target="_blank">
-              <FaGithub className="github" />
-              </a>
-              <a href="https://www.linkedin.com/in/HimanshuNarware/" target="_blank">
-              <FaLinkedin className="linkedin" />
-              </a>
-              <a href="https://twitter.com/N_Himanshu_" target="_blank">
-              <FaTwitter className="twittor" />
-              </a>
-            </div>
           </div>
+
           <div className="flex docGroup">
             <h4 className="doc">Documentation</h4>
             <div className="doc-pera">
@@ -41,11 +33,27 @@ function Footer() {
           </div>
           <div className="flex licenseGroup">
             <h3 className="lice ">License </h3>
-            <h5 className="license underline" style={{ cursor: 'pointer' }}>MIT License</h5>
+
+            <div className='licence'>
+            <h5 className="license underline" style={{ cursor: 'pointer' }}>MIT License | </h5>
             <h5 className="coc underline" style={{ cursor: 'pointer' }}>Code of Conduct</h5>
+            </div>
+            <div className="footer-socail-icon">
+              <a href="https://github.com/HimanshuNarware" target="_blank">
+              <FaGithub className="github" />
+              </a>
+              <a href="https://www.linkedin.com/in/HimanshuNarware/" target="_blank">
+              <FaLinkedin className="linkedin" />
+              </a>
+              <a href="https://twitter.com/N_Himanshu_" target="_blank">
+              <FaTwitter className="twittor" />
+              </a>
+            </div>
           </div>
 
-          <button size="size" type="defautl" className="button-star">
+          <div className='qrp'>
+            <img src={img}/>
+           <button size="size" type="defautl" className="button-star">
             <Link
               to="https://github.com/HimanshuNarware/CareerZunction_Intern"
               target='_blank'>
@@ -57,6 +65,8 @@ function Footer() {
               Star us
             </Link>
           </button>
+          </div>
+
         </div>
       </div>
     </div>
